d5c68414ac8e9a6e0afa44026da60724c686d08e
harrisjunaid <51511122+harrisjunaid@users.noreply.github.com>
2023-03-06 23:35:53 +0000

    01_03e updated

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -4,7 +4,17 @@ export default () => {
   const [count, setCount] = useState(0)
   return (
     <button className="outline" onClick={() => setCount(count + 1)}>
-      {count === 0 ? "Click to support": `Supported ${count} times`}
+      {/* Supported {count} */}
+
+      {(() => {
+        if (count === 0) {
+          return "Click to support";
+        } else {
+          return `Clicked: ${count}`
+        }
+      })()}
+      
+      {/* {count === 0 ? "Click to support": `Supported ${count} times`} */}
     </button>
   )
 }
